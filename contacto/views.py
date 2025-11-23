13bd00f4d97ad2bf0479d56cbfbfc309f8761950
Asus <estebancamilo718@gmail.com>
2025-11-23 10:15:51 -0500

    Fix: enviar formularios de contacto a mi correo

--- contacto/views.py
+++ contacto/views.py
@@ -3,31 +3,28 @@ from .forms import FormularioContacto
 from django.core.mail import EmailMessage
 from django.urls import reverse
 
-# Create your views here.
 def contacto(request):
     form = FormularioContacto(request.POST or None)
 
     if request.method == 'POST':
         if form.is_valid():
-            nombre   = form.cleaned_data['nombre']
-            correo   = form.cleaned_data['email']       
+            nombre = form.cleaned_data['nombre']
+            correo = form.cleaned_data['email']
             contenido = form.cleaned_data['contenido']
 
             msg = EmailMessage(
-                subject="Mensaje desde App Django",
-                body=f"El usuario {nombre} cuya dirección es {correo} escribe:\n\n{contenido}",
-                from_email="",                           
-                to=["genesisclothing04@gmail.com"],
+                subject="Mensaje desde la web TiendaGenesis",
+                body=f"El usuario {nombre} cuyo correo es {correo} escribió:\n\n{contenido}",
+                from_email="camilo.rubiano@estudiantesunibague.edu.co",   # <-- IMPORTANTÍSIMO
+                to=["camilo.rubiano@estudiantesunibague.edu.co"],         # <-- Te llegará a ti
                 reply_to=[correo],
             )
 
             try:
-                
                 msg.send(fail_silently=False)
                 return redirect(reverse('Contacto') + '?ok=1')
             except Exception as e:
-            
+                print("Error al enviar:", e)
                 return redirect(reverse('Contacto') + '?ok=0')
-
 
     return render(request, "contacto/contacto.html", {'formulario': form})
