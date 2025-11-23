7d2cce3bb9ad5cf885c153aecd31c890287f5131
Asus <estebancamilo718@gmail.com>
2025-11-23 07:33:04 -0500

    CloudinaryField en Post

--- informacion/models.py
+++ informacion/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
-
-# Create your models here.
+from cloudinary.models import CloudinaryField   # ✔ IMPORTANTE
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50, validators=[RegexValidator(
-    regex=r"^[A-Za-zÁÉÍÓÚáéíóúÜüÑñ' -]+$",
-    message="Solo se permiten letras, espacios, guiones y apóstrofes."
-)])
+        regex=r"^[A-Za-zÁÉÍÓÚáéíóúÜüÑñ' -]+$",
+        message="Solo se permiten letras, espacios, guiones y apóstrofes."
+    )])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -23,7 +22,10 @@ class Categoria(models.Model):
 class Post(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.TextField(max_length=100)
-    imagen = models.ImageField(upload_to="informacion", null=True, blank=True)
+
+    # ✔ AHORA USAMOS CLOUDINARY (igual que productos)
+    imagen = CloudinaryField('image', blank=True, null=True)
+
     autor = models.ForeignKey(User, on_delete=models.PROTECT)
     categorias = models.ManyToManyField(Categoria)
     created = models.DateTimeField(auto_now_add=True)
